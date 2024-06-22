2d7bae713e6c6f0f36c526cca2adbd8508a64a94
Feyzullah Durası <durasifeyzullah672@gmail.com>
2024-06-22 12:35:12 +0300

    Initial Commit

---
+++ FilmlerUygulaması/FilmlerUygulamas_App.swift
@@ -0,0 +1,17 @@
+//
+//  FilmlerUygulamas_App.swift
+//  FilmlerUygulaması
+//
+//  Created by Feyzullah Durası on 22.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct FilmlerUygulamas_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
